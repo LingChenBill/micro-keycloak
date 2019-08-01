a32912feeecfc673c13da1d13f56655d98220dbc
zhuyz <lingchen1316@163.com>
2019-08-01 22:16:26 +0800

    keycloak springboot init commit.

---
+++ src/main/java/com/lc/keycloak/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.lc.keycloak.controller;
+
+import com.lc.keycloak.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+/**
+ * product controller.
+ *
+ * @author zyz.
+ */
+@RestController
+public class ProductController {
+
+    /**
+     * 首页.
+     *
+     * @return
+     */
+    @GetMapping("/")
+    public String index() {
+        return "welcome to Keycloak index page!";
+    }
+
+    /**
+     * 获取产品.
+     *
+     * @return
+     */
+    @GetMapping("/product")
+    public Product getProduct() {
+        Product product = new Product();
+        product.setId("p-0001");
+        product.setName("iphone");
+        product.setPrice(new BigDecimal(5100.0));
+        return product;
+    }
+}
