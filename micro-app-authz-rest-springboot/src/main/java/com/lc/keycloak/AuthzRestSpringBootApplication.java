fb246fcacf4c16a6338d1c433221fcf668faf25a
lingchen1316@163.com <lingchen1316@163.com>
2019-08-06 19:09:47 +0800

    keycloak app-authz-rest-springboot commit.

---
+++ micro-app-authz-rest-springboot/src/main/java/com/lc/keycloak/AuthzRestSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.lc.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthzRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthzRestSpringBootApplication.class, args);
+	}
+}
